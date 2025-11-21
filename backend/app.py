c5d5d08f69c7f781591569add2c94f20ec28abe2
Sairaj <sairajdusane@gmail.com>
2025-11-21 17:19:39 +0530

    added new port in backend app.py

--- backend/app.py
+++ backend/app.py
@@ -247,5 +247,6 @@ def test_db():
         return {"status": "error", "message": str(e)}
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=int(os.getenv("PORT", 5000)))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
